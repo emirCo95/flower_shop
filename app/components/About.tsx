8616bc1a345669ac5a1343db22ef1dbf570e961e
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-10 18:26:32 -0500

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -10,17 +10,17 @@ const About = () => {
       <div className="w-full flex flex-col justify-center items-center">
         <h1 className="text-4xl p-5">SMELL THE BEAUTY</h1>
         <p className="w-[70%] text-center p-5">
-          Borcelle Boutique is a charming oasis where nature’s artistry meets
-          creative design. Each bouquet is crafted with care, bringing together
-          vibrant colors, unique textures, and delicate scents to create a work
-          of art for any occasion. From romantic roses and elegant lilies to
-          wildflowers and exotic orchids, the boutique offers a curated
-          selection that celebrates the beauty and versatility of flowers.
-          Knowledgeable florists provide personalized service, helping customers
-          select blooms that convey just the right message, whether it's love,
-          sympathy, or joy. The boutique itself is often styled with lush
-          greenery and elegant décor, making it a serene escape for anyone who
-          steps inside.
+          Borcelle Boutique is a charming oasis where nature&apos;s artistry
+          meets creative design. Each bouquet is crafted with care, bringing
+          together vibrant colors, unique textures, and delicate scents to
+          create a work of art for any occasion. From romantic roses and elegant
+          lilies to wildflowers and exotic orchids, the boutique offers a
+          curated selection that celebrates the beauty and versatility of
+          flowers. Knowledgeable florists provide personalized service, helping
+          customers select blooms that convey just the right message, whether
+          it&apos;s love, sympathy, or joy. The boutique itself is often styled
+          with lush greenery and elegant décor, making it a serene escape for
+          anyone who steps inside.
         </p>
       </div>
     </div>
